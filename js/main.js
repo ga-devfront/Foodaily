b53cee3a93296fea7b49898b862778fa84c7be09
ga-devfront <root@GameComputer.localdomain>
2020-02-19 13:39:07 +0100

    improve header creation

--- js/main.js
+++ js/main.js
@@ -35,40 +35,67 @@ class Site {
     }
   }
 
+  newHtml(settings) {
+    const newElement = $(`<${settings.element}>`);
+      $(newElement).attr(settings.attr);
+    for (let x = 0; settings.class.length > x; x += 1) {
+      $(newElement).addClass(settings.class[x]);
+    }
+    $(settings.parent).append(newElement);
+  }
+
   step0() {
     this.createLogo();
     this.createMenu();
   }
 
   createLogo() {
-    const logoLink = $('<a>');
-    const logoImg = $('<img>');
-    $('header').append(logoLink);
-    $(logoLink).append(logoImg);
-    $(logoLink).attr('href', '/foodaily');
-    $(logoImg).attr({
-      id: 'logo',
-      src: 'img/logo.png',
-      alt: 'logo Foodaily',
+    this.newHtml({
+      parent: $('header'),
+      element: 'a',
+      attr: {
+        href: '/foodaily'
+      },
+      class: [],
+    });
+    this.newHtml({
+      parent: $('header a'),
+      element: 'img',
+      attr: {
+        id: 'logo',
+        src: 'img/logo.png',
+        alt: 'logo Foodaily',
+      },
+      class: [],
     });
   }
 
   createMenu() {
-    const menu = $('<nav>');
-    const connect = $('<a>');
-    const subscribe = $('<a>');
-    $('header').append(menu);
-    $(menu).append(connect);
-    $(menu).append(subscribe);
-    $(connect).text('Se connecter');
-    $(connect).attr('id', 'connection');
-    $(subscribe).text('S\'inscrire');
-    $(subscribe).attr('id', 'subscribe');
-    $(subscribe).addClass('bold');
-  }
-
-  searchBar() {
-    
+    this.newHtml({
+      parent: $('header'),
+      element: 'nav',
+      attr: {
+      },
+      class: [],
+    });
+    this.newHtml({
+      parent: $('header nav'),
+      element: 'a',
+      attr: {
+        id: 'connection',
+      },
+      class: [],
+    });
+this.newHtml({
+      parent: $('header nav'),
+      element: 'a',
+      attr: {
+        id: 'subscribe',
+      },
+      class: ['bold'],
+    });
+    $('#connection').text('Se connecter');
+    $('#subscribe').text('S\'inscrire');
   }
 }
 
